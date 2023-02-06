8ca25b126a63a6a57d0ca00b60472e745dd7974a
john <you@example.com>
2023-02-05 23:56:17 -0600

    project init

---
+++ app/src/main/java/com/example/prework/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.prework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button=findViewById<Button>(R.id.IntroButton)
+        button.setOnClickListener {
+            Log.v("checker","test")
+            Toast.makeText(this,"Hello to You",Toast.LENGTH_SHORT).show()
+
+
+        }
+    }
+}
